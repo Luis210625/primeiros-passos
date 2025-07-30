35c05447fc3a9b2f94c6c4ee939aa039e0208c40
Luis210625 <luiscarlosmecca@proton.me>
2025-07-30 12:46:57 -0300

    Add files via upload

---
+++ segundo.py
@@ -0,0 +1,18 @@
+print("I'm Monty Python!!")
+print('\'\'\n V')
+print(True > False)
+print(True < False)
+
+print('\"Eu sou"')
+print('\""aprendizado""')
+print('\'"""Python"""')
+
+print("H", "E", "L", "L", "O", sep="-")
+
+print("Meu\nnome\né\nB", end=" ")
+print("\nJames Bond.")
+
+print("\n\npeixe", "salgadinhos", sep=" & ")
+
+print(2 + 2)
+
